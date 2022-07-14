cabccb3d977086230d2fd9d0535c76e1814084fe
mqjd <1378415278@qq.com>
2022-07-14 22:40:32 +0800

    bpmn-datamodel update

--- bpmn-datamodel/src/test/java/com/mqjd/datamodel/model/PojoFactoryTest.java
+++ bpmn-datamodel/src/test/java/com/mqjd/datamodel/model/PojoFactoryTest.java
@@ -1,12 +1,14 @@
 package com.mqjd.datamodel.model;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 import com.mqjd.datamodel.field.BasicField;
 import com.mqjd.datamodel.field.object.ObjectField;
 import com.mqjd.datamodel.schema.Schema;
 import com.mqjd.datamodel.utils.JsonUtils;
 import com.mqjd.validator.SchemaValidator;
-import org.junit.Assert;
-import org.junit.Test;
 
 public class PojoFactoryTest {
 
@@ -17,13 +19,13 @@ public class PojoFactoryTest {
         pojoConfig.setClassName("Test");
         BasicField basicField =
             JsonUtils.jsonToObject(getClass().getResourceAsStream("/model/object_schema.json"), BasicField.class);
-        Assert.assertTrue(basicField instanceof ObjectField);
+        assertTrue(basicField instanceof ObjectField);
         Schema schema = JsonUtils.jsonToObject(JsonUtils.toJson(basicField), Schema.class);
         pojoConfig.setSchema(schema);
         Class<?> aClass = PojoFactory.createPojo(pojoConfig, this.getClass().getClassLoader());
         SchemaValidator jsonObject =
             (SchemaValidator)JsonUtils.jsonToObject(getClass().getResourceAsStream("/model/object_value.json"), aClass);
         System.out.println(JsonUtils.toJson(jsonObject));
-        System.out.println(jsonObject.isValid());
+        assertTrue(jsonObject.isValid());
     }
 }
